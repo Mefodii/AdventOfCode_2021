bd320ddc45b9d409cf1bcbf820a0c86c203da72e
Mefodii <mefodii04@gmail.com>
2021-12-06 07:42:56 +0200

    Day 5 - 100%

--- days/day_5.py
+++ days/day_5.py
@@ -1,6 +1,6 @@
-import numpy, collections
-from re import match
-from functools import reduce
+import numpy
+import collections
+from re import match, findall
 
 
 class Map:
@@ -14,23 +14,40 @@ class Map:
 
         self.mask = numpy.full((width, height), 0)
 
-    def apply_linear_clouds(self):
+    def apply_clouds(self, diagonal=False):
         for cloud in self.clouds:
             x1, x2 = min(cloud.x1, cloud.x2), max(cloud.x1, cloud.x2)
             y1, y2 = min(cloud.y1, cloud.y2), max(cloud.y1, cloud.y2)
 
-            if x1 != x2 and y1 != y2:
-                continue
+            if x1 == x2 or y1 == y2:
+                self.apply_linear_cloud(x1, x2, y1, y2)
+            elif diagonal:
+                self.apply_diagonal_cloud(cloud.x1, cloud.x2, cloud.y1, cloud.y2)
 
-            for i in range(y1, y2 + 1):
-                for j in range(x1, x2 + 1):
-                    self.mask[i][j] += 1
+    def apply_linear_cloud(self, x1, x2, y1, y2):
+        for i in range(y1, y2 + 1):
+            for j in range(x1, x2 + 1):
+                self.mask[i][j] += 1
+
+    def apply_diagonal_cloud(self, x1, x2, y1, y2):
+        dist = abs(x2 - x1) + 1
+        x_sign = 1 if x2 > x1 else -1
+        y_sign = 1 if y2 > y1 else -1
+
+        for i in range(dist):
+            x = x1 + (i * x_sign)
+            y = y1 + (i * y_sign)
+            self.mask[y][x] += 1
 
     def calc_max_overlap(self):
         self.max_overlap = max([max(row) for row in self.mask])
 
-    def cal_max_overlap_count(self):
-        self.max_overlap_count = sum(collections.Counter(row)[self.max_overlap] for row in self.mask)
+    def calc_overlap_count(self):
+        for row in self.mask:
+            counter = collections.Counter(row)
+            for overlap, count in counter.items():
+                if overlap >= 2:
+                    self.max_overlap_count += count
 
     def __str__(self):
         result = ""
@@ -44,11 +61,11 @@ class Map:
 class Cloud:
 
     def __init__(self, data):
-        matcher = match(r'(\d+),(\d+).*(\d+),(\d+)', data)
-        self.x1 = int(matcher.group(1))
-        self.y1 = int(matcher.group(2))
-        self.x2 = int(matcher.group(3))
-        self.y2 = int(matcher.group(4))
+        result = findall(r'(\d+)', data)
+        self.x1 = int(result[0])
+        self.y1 = int(result[1])
+        self.x2 = int(result[2])
+        self.y2 = int(result[3])
 
     def __str__(self):
         return f"{self.x1},{self.y1} -> {self.x2},{self.y2}"
@@ -63,19 +80,20 @@ def build_cloud_map(data: list):
         max_y = max(max_y, cloud.y1, cloud.y2)
 
     _map = Map(max_y + 1, max_x + 1, clouds)
-    _map.apply_linear_clouds()
-    _map.calc_max_overlap()
-    _map.cal_max_overlap_count()
 
-    print(_map.max_overlap, _map.max_overlap_count)
     return _map
 
 
 ###############################################################################
 def run_a(input_data):
     _map = build_cloud_map(input_data)
-    return ""
+    _map.apply_clouds()
+    _map.calc_overlap_count()
+    return _map.max_overlap_count
 
 
 def run_b(input_data):
-    return ""
+    _map = build_cloud_map(input_data)
+    _map.apply_clouds(diagonal=True)
+    _map.calc_overlap_count()
+    return _map.max_overlap_count
