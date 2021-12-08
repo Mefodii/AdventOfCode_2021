cfba41d191a7754d2dfdfcb51d5b42ca89da2955
Mefodii <mefodii04@gmail.com>
2021-12-08 08:49:13 +0200

    subtract_string_list function

--- utils/manipulators.py
+++ utils/manipulators.py
@@ -1,2 +1,9 @@
+from collections import Counter
+
+
 def list_to_int(data):
     return list(map(int, data))
+
+
+def subtract_string_list(list1, list2):
+    return list((Counter(list1)-Counter(list2)).elements())
